a06acd1020ed37a443ac14e7f96051947b4c8c5b
s3660457 <unconfigured@null.spigotmc.org>
2020-03-29 12:25:51 +1100

    Created game.py

---
+++ game.py
@@ -0,0 +1,19 @@
+from sense_hat import SenseHat
+import electronicDie
+
+sense = SenseHat()
+sense.clear()
+
+# Player Scores
+p1_score = 0
+p2_score = 0
+
+# Current player turn
+player_turn = 1
+
+while p1_score < 30 and p2_score < 30:
+    if player_turn == 1:
+        player_turn + 1
+    else:
+        player_turn - 1
+        
