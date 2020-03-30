1b01207894c694a8f11e529757d3b17324fd5194
s3660457 <unconfigured@null.spigotmc.org>
2020-03-30 12:59:59 +1100

    Added file writer

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from sense_hat import SenseHat
 from electronicDie import roll_dice
+import csv, time
 
 sense = SenseHat()
 sense.clear()
@@ -8,8 +9,26 @@ sense.clear()
 p1_score = 0
 p2_score = 0
 
+f = open('winner.csv', 'w') # Open file
+
 # Current player turn
 player_turn = 1
+
+def save_winner(score):
+    with f:
+        writer = csv.writer(f)
+
+        t = time.localtime() # Get current time
+        current_time = time.strftime("%H:%M:%S", t) # Format time
+
+        writer.writerow(score + current_time)
+
+
+def check_player_score(score):
+    if score == 30:
+        sense.show_message("Player " + player_turn + " Wins  ")
+    else:
+        pass
 
 while p1_score < 30 and p2_score < 30:
     roll = 0 # Reset roll each time
@@ -25,10 +44,4 @@ while p1_score < 30 and p2_score < 30:
         p2_score + roll
         check_player_score(p2_score)
         player_turn - 1
-
-def check_player_score(score):
-    if score == 30:
-        sense.show_message("Player " + player_turn + " Wins  ")
-    else:
-        pass
         
