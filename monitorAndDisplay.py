6970f84fb25fa65499da70295f20188c5db6f69e
s3660457 <31838937+s3660457@users.noreply.github.com>
2020-04-02 19:23:22 +1100

    Changed display types

--- monitorAndDisplay.py
+++ monitorAndDisplay.py
@@ -8,13 +8,13 @@ file_contents = [] #storing the current file contents
 
 # use moving average to smooth readings
 def get_smooth(x):
-  if not hasattr(get_smooth, "t"):
-    get_smooth.t = [x,x,x]
-  get_smooth.t[2] = get_smooth.t[1]
-  get_smooth.t[1] = get_smooth.t[0]
-  get_smooth.t[0] = x
-  xs = (get_smooth.t[0]+get_smooth.t[1]+get_smooth.t[2])/3
-  return(xs)
+    if not hasattr(get_smooth, "t"):
+        get_smooth.t = [x,x,x]
+    get_smooth.t[2] = get_smooth.t[1]
+    get_smooth.t[1] = get_smooth.t[0]
+    get_smooth.t[0] = x
+    xs = (get_smooth.t[0]+get_smooth.t[1]+get_smooth.t[2])/3
+    return(xs)
 
 # Get CPU temperature
 def get_cpu_temp():
@@ -52,7 +52,7 @@ def get_current_temp():
 # Set led based on temp
 def set_led():
     colour = (255,255,255)
-    realTemp = get_current_temp()
+    realTemp = round(get_current_temp())
     if realTemp <= file_contents[0]:
         colour = (0,0,255) #blue
         # sense.clear(b)
@@ -62,9 +62,9 @@ def set_led():
     else:
         colour = (255,0,0) #green
         # sense.clear(g)
-    sense.show_message(round(realTemp), text_colour=colour)
+    sense.show_message(realTemp, text_colour=colour)
 
 while True:
     get_file_contents()
-    get_current_temp()
+    set_led()
     time.sleep(10)
