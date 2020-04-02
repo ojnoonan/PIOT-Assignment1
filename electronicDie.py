0edd9564c8fb1d7aaeb61e96b9b4215b7188258e
s3660457 <31838937+s3660457@users.noreply.github.com>
2020-04-02 20:28:29 +1100

    fixed minor issues

--- electronicDie.py
+++ electronicDie.py
@@ -79,48 +79,50 @@ SIX = [
   g,g,b,b,b,b,g,g,
   g,g,b,b,b,b,g,g,
   ]
-def roll_dice():
-  r = random.randint(1,6) 
-  p = r%6
-  n = 0
-   
-  if p == 1:
-      sense.set_pixels(ONE)
-      n = 1
-  elif p == 2:
-      sense.set_pixels(TWO)
-      n = 2
-  elif p == 3:
-      sense.set_pixels(THREE)
-      n = 3
-  elif p == 4:
-      sense.set_pixels(FOUR)
-      n = 4
-  elif p == 5:
-      sense.set_pixels(FIVE)
-      n = 5
-  elif p == 0:
-      sense.set_pixels(SIX)
-      n = 6
-  return n
+class emoJ:
+  
+    def roll_dice(s):
+      r = random.randint(1,6) 
+      p = r%6
+      n = 0
+       
+      if p == 1:
+          sense.set_pixels(ONE)
+          n = 1
+      elif p == 2:
+          sense.set_pixels(TWO)
+          n = 2
+      elif p == 3:
+          sense.set_pixels(THREE)
+          n = 3
+      elif p == 4:
+          sense.set_pixels(FOUR)
+          n = 4
+      elif p == 5:
+          sense.set_pixels(FIVE)
+          n = 5
+      elif p == 0:
+          sense.set_pixels(SIX)
+          n = 6
+      return n
 
-def check_for_movement(loop_break):
-  sense.show_message("Shake  ")
-  while True:
-      x, y, z = sense.get_accelerometer_raw().values()
+    def check_for_movement(loop_break, s):
+      sense.show_message("Shake  ")
+      while True:
+          x, y, z = sense.get_accelerometer_raw().values()
 
-      x = abs(x)
-      y = abs(y)
-      z = abs(z)
+          x = abs(x)
+          y = abs(y)
+          z = abs(z)
 
-      if x > 1.5 or y > 1.5 or z > 1.5:
-          roll = roll_dice()
-          time.sleep(1)
-          if loop_break is True:
-              return roll
-              break
-
+          if x > 1.5 or y > 1.5 or z > 1.5:
+              roll = dice.roll_dice() 
+              time.sleep(1)
+              if loop_break is True:
+                  return roll
+                  break
+dice = emoJ()
 if __name__ == "__main__":
-    check_for_movement(loop_break)
-    roll_dice()
+    dice.check_for_movement(loop_break)
+    dice.roll_dice()
 
