593b74879b44cc4851b5ff8a554c6c4f1b8e1b85
s3660457 <s3660457@student.rmit.edu.au>
2020-04-04 21:53:53 +1100

    Added Class

--- game.py
+++ game.py
@@ -14,39 +14,42 @@ f = open('winner.csv', 'w') # Open file
 # Current player turn
 player_turn = 1
 
-def save_winner(score):
-    with f:
-        writer = csv.writer(f)
+class diceGame:
 
-        t = time.localtime() # Get current time
-        current_time = time.strftime("%H:%M:%S", t) # Format time
+    def save_winner(self, score):
+        with f:
+            writer = csv.writer(f)
 
-        writer.writerow(current_time + " " + str(score))
+            t = time.localtime() # Get current time
+            current_time = time.strftime("%H:%M:%S", t) # Format time
+
+            writer.writerow(current_time + " " + str(score))
 
 
-def check_player_score(score):
-    print(score)
-    if score >= 30:
-        sense.show_message("Player " + str(player_turn) + " Wins  ")
-        save_winner(score)
-    else:
-        pass
+    def check_player_score(self, score):
+        print(score)
+        if score >= 30:
+            sense.show_message("Player " + str(player_turn) + " Wins  ")
+            dg.save_winner(score)
+        else:
+            pass
 
-while p1_score < 30 and p2_score < 30:
-    die = emoJ(True)
-    roll = 0 # Reset roll each time
-    if player_turn == 1:
-        sense.show_message("Player " + str(player_turn), scroll_speed=0.05)
-        roll = die.check_for_movement()
-        p1_score += roll
-        check_player_score(p1_score)
-        player_turn += 1
+    while p1_score < 30 and p2_score < 30:
+        die = emoJ(True)
+        roll = 0 # Reset roll each time
+        if player_turn == 1:
+            sense.show_message("Player " + str(player_turn), scroll_speed=0.05)
+            roll = die.check_for_movement()
+            p1_score += roll
+            dg.check_player_score(p1_score)
+            player_turn += 1
 
-    else:
-        sense.show_message("Player " + str(player_turn), scroll_speed=0.05)
-        roll = die.check_for_movement()
-        p2_score += roll
-        check_player_score(p2_score)
-        player_turn -= 1
+        else:
+            sense.show_message("Player " + str(player_turn), scroll_speed=0.05)
+            roll = die.check_for_movement()
+            p2_score += roll
+            dg.check_player_score(p2_score)
+            player_turn -= 1
+dg = diceGame()
 
 
