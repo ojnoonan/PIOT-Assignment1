2d35e97eab0afb6ba64b5c2d60afcd9ffa9ae866
s3660457 <31838937+s3660457@users.noreply.github.com>
2020-04-05 13:55:28 +1000

    Updated version with working class

--- game.py
+++ game.py
@@ -9,47 +9,47 @@ sense.clear()
 p1_score = 0
 p2_score = 0
 
-f = open('winner.csv', 'w') # Open file
+f = open('winner.csv', 'a') # Open file
 
 # Current player turn
 player_turn = 1
 
+# Saves the winning players score to the file
+def save_winner(score):
+    with f:
+        writer = csv.writer(f)
+
+        t = time.localtime() # Get current time
+        current_time = time.strftime("%H:%M:%S", t) # Format time
+
+        writer.writerow(current_time + " " + str(score))
+
 class diceGame:
 
-    def save_winner(self, score):
-        with f:
-            writer = csv.writer(f)
-
-            t = time.localtime() # Get current time
-            current_time = time.strftime("%H:%M:%S", t) # Format time
-
-            writer.writerow(current_time + " " + str(score))
-
-
-    def check_player_score(self, score):
-        print(score)
-        if score >= 30:
+    def check_player_score(score, player_turn):
+        print(str(score))
+        if score >= 10:
             sense.show_message("Player " + str(player_turn) + " Wins  ")
-            dg.save_winner(score)
+            save_winner(score)
         else:
             pass
 
-    while p1_score < 30 and p2_score < 30:
+    while p1_score < 10 and p2_score < 10:
+        print(player_turn)
         die = emoJ(True)
         roll = 0 # Reset roll each time
         if player_turn == 1:
             sense.show_message("Player " + str(player_turn), scroll_speed=0.05)
             roll = die.check_for_movement()
             p1_score += roll
-            dg.check_player_score(p1_score)
+            check_player_score(p1_score, player_turn)
             player_turn += 1
 
         else:
             sense.show_message("Player " + str(player_turn), scroll_speed=0.05)
             roll = die.check_for_movement()
             p2_score += roll
-            dg.check_player_score(p2_score)
+            check_player_score(p2_score, player_turn)
             player_turn -= 1
-dg = diceGame()
 
-
+diceGame()
